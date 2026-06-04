cd0d279076c4a856a26081e083465b61ea741041
aquinomark217 <aquinomark217@gmail.com>
2026-06-04 23:36:35 +0800

    Configure database connection using environment variables

--- api/config/db.js
+++ api/config/db.js
@@ -1,10 +1,12 @@
 const mysql = require("mysql2/promise");
+require("dotenv").config();
 
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "coffee_shop_db",
+  host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
