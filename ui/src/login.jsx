762e962a185b13c37961a778c0359034a635e1f5
aquinomark217 <aquinomark217@gmail.com>
2026-06-05 00:30:55 +0800

    Update login API endpoint to a shorter path

--- ui/src/login.jsx
+++ ui/src/login.jsx
@@ -23,7 +23,7 @@ function Login({ setUser }) {
     e.preventDefault();
 
     try {
-      const res = await fetch(`${BASE_URL}/api/messages/auth/login`, {
+      const res = await fetch(`${BASE_URL}/auth/login`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
